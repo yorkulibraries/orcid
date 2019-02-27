c5a02dd15fba9fe84cd12ce64ce0e6b9201c2289
Tuan Nguyen <amtuannguyen@gmail.com>
2019-02-27 08:55:05 -0500

    use nodesource for nodejs to get the latest version

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ sudo timedatectl set-timezone America/Toronto
 sudo apt-get update
 sudo apt-get dist-upgrade
 
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+
 sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev git build-essential libxml2 libxml2-dev libxslt-dev libsqlite3-dev sqlite3 nodejs
 
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
