ea85748446c321180aea7ffed9e0f606da75dea3
Tuan Nguyen <amtuannguyen@gmail.com>
2019-03-07 07:57:36 -0500

    update token in omniauth callback

--- orcid/app/controllers/users/omniauth_callbacks_controller.rb
+++ orcid/app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     Rails.logger.debug "auth info from omniauth: " + request.env["omniauth.auth"].to_yaml
           
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    
+    @user.token = request.env["omniauth.auth"].credentials.token
+    @user.refresh_token = request.env["omniauth.auth"].credentials.refresh_token
+    @user.token_expires_at = request.env["omniauth.auth"].credentials.expires_at
+    @user.save
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
     end     
