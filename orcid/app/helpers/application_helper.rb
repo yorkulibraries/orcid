82da885cdb35ebf829d0be3b3e4f5a3f20ef401c
Tuan Nguyen <amtuannguyen@gmail.com>
2019-07-05 08:21:19 -0400

    change orcid authorize scope to /authenticate

--- orcid/app/helpers/application_helper.rb
+++ orcid/app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     base_url = ENV['ORCID_SANDBOX'] == "true" ? "https://sandbox.orcid.org" : "https://orcid.org"
      
     base_url + '/oauth/authorize?client_id=' + ENV['ORCID_CLIENT_ID'] \
-    + '&response_type=code&scope=/read-limited%20/person/update%20/activities/update&redirect_uri=' \
+    + '&response_type=code&scope=/authenticate&redirect_uri=' \
     + url_for(action: 'orcid', controller: 'users/omniauth_callbacks', only_path: false) \
     + '&state=' + session['omniauth.state'] 
   end
